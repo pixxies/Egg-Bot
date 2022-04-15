c520d9069228ba0944d93969e0446fdc6c21ac9d
pixxies <hello@pixxiesdesign.com>
2022-04-15 15:04:40 +0200

    Set status

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,14 @@ module.exports = {
     name: 'ready',
     async execute(client) {
 
+        client.user.setPresence({
+            activities: [{
+                name: "in the Egg Hunt | /basket",
+                type: 'COMPETING'
+            }],
+            status: 'online'
+        });
+
         console.log(`${client.user.tag} is online!`)
 
     }
